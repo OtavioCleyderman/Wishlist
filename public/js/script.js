076e895241300ec11e3e9645b421d3c7f0a153f8
Otávio Cleyderman Ferraz Faria <68860740+OtavioCleyderman@users.noreply.github.com>
2022-06-19 00:03:06 -0300

    (19/06) Realizado consulta na API e iniciado construção e layout dos cards

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,6 @@
+const favorite = document.querySelector('.card-wishlist')
+const heartWishlist = document.querySelector('.bi-heart-fill')
 
-
+favorite.addEventListener('click', () => {
+  heartWishlist.classList.toggle('selected')
+})
---
+++ route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios')
+const route = express.Router();
+
+
+route.get('/', async (req, res) => {
+
+  // Como no axios já vem o response, então já tiro o data direto do response
+  const { data } = await axios('https://run.mocky.io/v3/66063904-d43c-49ed-9329-d69ad44b885e')
+  res.render('index', {page: 'home', products: data.products})
+  
+})
+
+route.get('/wishlist', (req, res) => {
+  res.render('index', {page: 'wishlist'})
+})
+
+// route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
+// route.get('/create-pass', (req, res) => res.render('index', { page: 'create-pass' }))
+
+
+module.exports = route
