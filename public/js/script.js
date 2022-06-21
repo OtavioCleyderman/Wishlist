0d660220001a73777b2fcb2e3171237087436741
Otávio Cleyderman Ferraz Faria <68860740+OtavioCleyderman@users.noreply.github.com>
2022-06-21 09:12:54 -0300

    (21/06) Concluido o layout das paginas e funcionalidade de adicionar e remover produto da wishlist

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,11 @@
 const card = document.querySelectorAll('.card')
 const wishlistButton = document.querySelectorAll('.card-wishlist')
+const wishlistButtonDelete = document.querySelectorAll('.iconDeleteWishlist')
 const heartIcon = document.querySelectorAll('.bi-heart-fill')
 let wishlist = JSON.parse(localStorage.getItem('wishlist')) || []
 let heartIconSelected = JSON.parse(localStorage.getItem('heartIconSelected')) || []
 
-function wishlistUpdate() {
+function wishlistUpdateHome() {
   for(let i = 0; i < wishlistButton.length; i++){
 
     // Pegar os dados de cada card, dados: Imagem, titulo e preço. Precisarei para inserir no localStorage e depois pegar esses dados e renderizar na tela wishlist
@@ -17,7 +18,7 @@ function wishlistUpdate() {
     const index = wishlist.findIndex(element => element.title === title & element.price === price)
     
     // Caso tenha produtos no localStorage, ou seja, na wishlist do cliente, na página de produtos reflete qual está em sua lista também
-    if(index !== -1) {
+    if(index != -1) {
       heartIcon[i].style.fill = 'red'
     } else {
       heartIcon[i].style.fill = '#FFF'
@@ -25,24 +26,41 @@ function wishlistUpdate() {
 
     // Já com o seu click, consegue adicionar ou remover produtos da sua wishlist direto da página de produtos
     wishlistButton[i].addEventListener('click', () => {
-      if(index !== -1) {
-        wishlist.splice(index, 1)
+      const idx = wishlist.findIndex(element => element.title === title & element.price === price)
+      if(idx != -1) {
+        wishlist.splice(idx, 1)
         heartIcon[i].style.fill = '#FFF'
+        console.log(idx)
+        console.log(title, price)
       } else {
         wishlist.push({imageSrc, title, price})
         heartIcon[i].style.fill = 'red'
+        console.log(idx)
+        console.log(title, price)
       } /* Depois verificar como refatorar os dois blocos if acima*/
   
+
       // inserindo o produto escolhido pelo cliente para sua wishlist no localStorage
       localStorage.setItem('wishlist', JSON.stringify(wishlist))
-  
     })
   }
 }
 
-wishlistUpdate()
+wishlistUpdateHome()
 
 
+function wishlistRemove() {
+  for(let i = 0; i < wishlist.length; i++){
+    wishlistButtonDelete[i].addEventListener('click', () => {
+      const index = wishlist.findIndex(element => element.title == wishlist[i].title )
+      wishlist.splice(index, 1)
+      localStorage.setItem('wishlist', JSON.stringify(wishlist))
+      location.reload()
+    })
+  }
+}
+
+wishlistRemove()
 
 
 
